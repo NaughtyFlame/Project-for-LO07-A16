ea07a296086fd3e2e0257e1178b31fc917c2793d
sophiezhf <haifan.zhang1221@gmail.com>
2016-05-17 02:04:01 +0200

    insert data

--- sql/BDD_publication_create.sql
+++ sql/BDD_publication_create.sql
@@ -1,23 +1,29 @@
-CREATE TABLE pubilication
+CREATE TABLE publication
 (
 	no_p int NOT NULL PRIMARY KEY,
 	titre VARCHAR(255) NOT NULL,
-	revenu VARCHAR(255),
+        categorie VARCHAR(255),
 	annee YEAR,
-	/*url VARCHAR(255),*/
-        lieu_confer VARCHAR(255),
-        status VARCHAR(255),
-        categorie VARCHAR(255)
+	url VARCHAR(255),
+        lieu_conference VARCHAR(255),/*uniqm valable pour les conference */
+        status VARCHAR(255),/*soumis, en révision,publié*/
+        foreign key (no_r) references revue(no_r)
 );
+
+create table revue 
+(
+    no_r int not null primary key,
+    nom_r varchar(255)
+)
 
 CREATE TABLE auteur
 (
 	no_ss int NOT NULL PRIMARY KEY,
 	nom VARCHAR(64) NOT NULL,
 	prenom VARCHAR(64),
-        groupe VARCHAR(255) 
+        organisation VARCHAR(255)
 );
-
+    
 CREATE TABLE ecrire
 (
 	id_p int NOT NULL,
@@ -25,10 +31,12 @@ CREATE TABLE ecrire
 	CONSTRAINT pk_ecrire PRIMARY KEY(id_p,id_auteur)
 );
 
-CREATE TABLE affiliation
+CREATE TABLE affiliation/*equipe*/
 (
 	no_aff int NOT NULL PRIMARY KEY,
-	nom VARCHAR(255) NOT NULL
+	nom_a VARCHAR(255) NOT NULL,
+        foreign key (no_ss) refenrences auteur(no_ss)
+       
 );
 
 CREATE TABLE afficher
@@ -38,10 +46,10 @@ CREATE TABLE afficher
 	CONSTRAINT pk_afficher PRIMARY KEY(id_p,id_aff)
 );
 
-CREATE TABLE utlisation
+CREATE TABLE utilisation
 (  
     username VARCHAR(64) NOT NULL PRIMARY KEY,
-    pass VARCHAR(64) NOT NULL,
+    passwd VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     firstname VARCHAR(64) NOT NULL,
     lastname VARCHAR(64) NOT NULL
--- sql/BDD_publication_insert.sql
+++ sql/BDD_publication_insert.sql
@@ -7,4 +7,60 @@
  * Author:  Haifan ZHANG
  * Created: 2016-5-13
  */
+/*ajouter les infos pour les auteur et les universités*/
+/*insert into auteur values (1, "Allais","Romain","UTT");
+insert into auteur values (2, "Allouche","Sylvie","UTC");
+insert into auteur values (3, "Aguert","Marc","UTBM");
+insert into auteur values (4, "Hachet","Laurent","UTSEUS");
+insert into auteur values (5, "Araud","Christian","UTT");
+insert into auteur values (6, "Atifi","Hassan","UTC");
+insert into auteur values (7, "Begriche","Philippe","UTT");
+insert into auteur values (8, "Belamri","Philippe","UTT");
+insert into auteur values (9, "Belamri","Lucile","UTC");
+insert into auteur values (10, "Bennis","Bruno","UTBM");
+insert into auteur values (11, "Billard","Jean-louis","UTSEUS");
+insert into auteur values (12, "Brecht","Daniel","UTT");
+insert into auteur values (13, "Brasil","Sophie","UTC");
+insert into auteur values (14, "Bullot","Florie","UTBM");
+insert into auteur values (15, "Casas","Michel","UTSEUS");
+insert into auteur values (16, "Castro","Rafeal","UTT");
+insert into auteur values (17, "Chabbat","Elodie","UTC");
+insert into auteur values (18, "Châtelet","Luc","UTC")*/
+
+/*ajouter les infos pour Publications*/
+/*
+insert into publication (no_p, titre, annee,status) values (1,"Communautés de pratique et performance",2011,"publié");
+insert into publication (no_p, titre, annee,status) values (2,"A model to quantify the resilience of mass railway transportation systems",2012,"en révision");
+insert into publication (no_p, titre, annee,status) values (3,"Multi-task learning with one-class SVM",2013,"soumis");
+insert into publication (no_p, titre, annee,lieu_conference,status) values (4,"Analyse des données pour la notation de lots",2014,"IEEE International Conference on Image Processing","publié");
+insert into publication (no_p, titre, annee,status) values (5,"Controlled systems, failure prediction and maintenance",2015,"en révision");
+insert into publication (no_p, titre, annee,status) values (6,"Multi-Constellation GNSS based on next generation RAIM",2016,"soumis");
+insert into publication (no_p, titre, annee,lieu_conference,status) values (7,"A deterioration model for feedback control systems with random environment",2010,"22nd Annual Conference ESREL on European Safety and Reliability","publié");
+insert into publication (no_p, titre, annee,lieu_conference,status) values (8,"Degradation prognosis based on a model of Gamma process mixture",2009,"Proc. 24th IEEE workshop on Machine Learning for Signal Processing","publié");
+insert into publication (no_p, titre, annee,status) values (9,"Mahalanobis-Based One-Class Classification",2008,"publié");
+insert into publication (no_p, titre, annee,lieu_conference,status) values (10,"Privacy Threats from Social Networking Service Aggregators",2007,"5th Cybercrime and Trustworthy Computing Conference","publié");
+insert into publication (no_p, titre, annee,status) values (11,"Rich Model for Steganalysis of Color Images",2006,"en révision");
+insert into publication (no_p, titre, annee,status) values (12,"Simulation of image time series from dynamical fractional brownian fields",2005,"soumis")
+*/
+/*ajouter les infos pour les journaux
+insert into revue values (1,"Systèmes d’information et Management");
+insert into revue values (2,"Reliability Engineering & System Safety");
+insert into revue values (3,"Neurocomputing");
+insert into revue values (4,"Proceedings of the Ninth International Conference on Mathematical Methods in Reliability");
+insert into revue values (5,"Proceedings of the European Navigation Conference");
+insert into revue values (6,"PHM");
+insert into revue values (7,"IEEE");
+insert into revue values (8,"Journal de la Société Française de Statistique")
+*/
+
+/*ajouter les infos pour organisation*/
+insert into affiliation (no_aff,nom_a) values (1,"CREIDD");
+insert into affiliation (no_aff,nom_a) values (2,"ERA");
+insert into affiliation (no_aff,nom_a) values (3,"GAMMA3");
+insert into affiliation (no_aff,nom_a) values (4,"LASMIS");
+insert into affiliation (no_aff,nom_a) values (5,"LM2S");
+insert into affiliation (no_aff,nom_a) values (6,"LNIO");
+insert into affiliation (no_aff,nom_a) values (7,"LOSI");
+insert into affiliation (no_aff,nom_a) values (8,"Tech-CICO");
+
 
