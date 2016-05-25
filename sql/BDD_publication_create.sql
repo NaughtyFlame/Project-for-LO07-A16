b226ee42bc26c5a1162aa29ed66535b57b2a4095
sophiezhf <haifan.zhang1221@gmail.com>
2016-05-25 16:16:05 +0200

    structure de base de donnée simplifiée
    bingo

--- sql/BDD_publication_create.sql
+++ sql/BDD_publication_create.sql
@@ -1,56 +1,54 @@
 CREATE TABLE publication
 (
-	no_p int NOT NULL PRIMARY KEY,
+	id_p int NOT NULL PRIMARY KEY,
 	titre VARCHAR(255) NOT NULL,
-        categorie VARCHAR(255),
-	annee YEAR,
-	url VARCHAR(255),
-        lieu_conference VARCHAR(255),/*uniqm valable pour les conference */
-        status VARCHAR(255),/*soumis, en révision,publié*/
-        foreign key (no_r) references revue(no_r)
+        auteurs varchar(255),
+        annee int,
+        titre_conference varchar(255),
+        lieu_conference varchar(255)
 );
 
-create table revue 
+create table chercheur
 (
-    no_r int not null primary key,
-    nom_r varchar(255)
+    id_ch int not null primary key,
+    nom_ch varchar(255),
+    prenom_ch varchar(255),
+    id_org int,
+    foreign key (id_org) references organisation (id_org),
+    id_eq int,
+    foreign key (id_eq) references equipe (id_eq)
+);
+
+create table utilisateur 
+(
+    id_ut int not null primary key,
+    nom_ut varchar(64)not null,
+    prenom_ut varchar(64) not null,
+    mail varchar(255),
+    login varchar(255),
+    pwd varchar(64)
+ );
+
+create table categorie
+(
+    id_cat int not null primary key,
+    nom_cat varchar(255) not null,
+    id_p int,
+    foreign key (id_p) references publication  (id_p)
+);
+
+create table equipe
+(
+    id_eq int not null primary key,
+    nom_equipe varchar(64),
+    id_ch int,
+    foreign key (id_ch) references chercheur (id_ch)
+);
+
+create table organisation
+(
+    id_org int not null primary key,
+    nom_org varchar(64),
+    id_ch int,
+    foreign key (id_ch) references chercheur (id_ch)
 )
-
-CREATE TABLE auteur
-(
-	no_ss int NOT NULL PRIMARY KEY,
-	nom VARCHAR(64) NOT NULL,
-	prenom VARCHAR(64),
-        organisation VARCHAR(255)
-);
-    
-CREATE TABLE ecrire
-(
-	id_p int NOT NULL,
-	id_auteur int NOT NULL,
-	CONSTRAINT pk_ecrire PRIMARY KEY(id_p,id_auteur)
-);
-
-CREATE TABLE affiliation/*equipe*/
-(
-	no_aff int NOT NULL PRIMARY KEY,
-	nom_a VARCHAR(255) NOT NULL,
-        foreign key (no_ss) refenrences auteur(no_ss)
-       
-);
-
-CREATE TABLE afficher
-(
-	id_p int NOT NULL,
-	id_aff int NOT NULL,
-	CONSTRAINT pk_afficher PRIMARY KEY(id_p,id_aff)
-);
-
-CREATE TABLE utilisation
-(  
-    username VARCHAR(64) NOT NULL PRIMARY KEY,
-    passwd VARCHAR(64) NOT NULL,
-    email VARCHAR(64) NOT NULL,
-    firstname VARCHAR(64) NOT NULL,
-    lastname VARCHAR(64) NOT NULL
-);
--- sql/BDD_publication_insert.sql
+++ sql/BDD_publication_insert.sql
@@ -7,60 +7,84 @@
  * Author:  Haifan ZHANG
  * Created: 2016-5-13
  */
-/*ajouter les infos pour les auteur et les universités*/
-/*insert into auteur values (1, "Allais","Romain","UTT");
-insert into auteur values (2, "Allouche","Sylvie","UTC");
-insert into auteur values (3, "Aguert","Marc","UTBM");
-insert into auteur values (4, "Hachet","Laurent","UTSEUS");
-insert into auteur values (5, "Araud","Christian","UTT");
-insert into auteur values (6, "Atifi","Hassan","UTC");
-insert into auteur values (7, "Begriche","Philippe","UTT");
-insert into auteur values (8, "Belamri","Philippe","UTT");
-insert into auteur values (9, "Belamri","Lucile","UTC");
-insert into auteur values (10, "Bennis","Bruno","UTBM");
-insert into auteur values (11, "Billard","Jean-louis","UTSEUS");
-insert into auteur values (12, "Brecht","Daniel","UTT");
-insert into auteur values (13, "Brasil","Sophie","UTC");
-insert into auteur values (14, "Bullot","Florie","UTBM");
-insert into auteur values (15, "Casas","Michel","UTSEUS");
-insert into auteur values (16, "Castro","Rafeal","UTT");
-insert into auteur values (17, "Chabbat","Elodie","UTC");
-insert into auteur values (18, "Châtelet","Luc","UTC")*/
-
-/*ajouter les infos pour Publications*/
-/*
-insert into publication (no_p, titre, annee,status) values (1,"Communautés de pratique et performance",2011,"publié");
-insert into publication (no_p, titre, annee,status) values (2,"A model to quantify the resilience of mass railway transportation systems",2012,"en révision");
-insert into publication (no_p, titre, annee,status) values (3,"Multi-task learning with one-class SVM",2013,"soumis");
-insert into publication (no_p, titre, annee,lieu_conference,status) values (4,"Analyse des données pour la notation de lots",2014,"IEEE International Conference on Image Processing","publié");
-insert into publication (no_p, titre, annee,status) values (5,"Controlled systems, failure prediction and maintenance",2015,"en révision");
-insert into publication (no_p, titre, annee,status) values (6,"Multi-Constellation GNSS based on next generation RAIM",2016,"soumis");
-insert into publication (no_p, titre, annee,lieu_conference,status) values (7,"A deterioration model for feedback control systems with random environment",2010,"22nd Annual Conference ESREL on European Safety and Reliability","publié");
-insert into publication (no_p, titre, annee,lieu_conference,status) values (8,"Degradation prognosis based on a model of Gamma process mixture",2009,"Proc. 24th IEEE workshop on Machine Learning for Signal Processing","publié");
-insert into publication (no_p, titre, annee,status) values (9,"Mahalanobis-Based One-Class Classification",2008,"publié");
-insert into publication (no_p, titre, annee,lieu_conference,status) values (10,"Privacy Threats from Social Networking Service Aggregators",2007,"5th Cybercrime and Trustworthy Computing Conference","publié");
-insert into publication (no_p, titre, annee,status) values (11,"Rich Model for Steganalysis of Color Images",2006,"en révision");
-insert into publication (no_p, titre, annee,status) values (12,"Simulation of image time series from dynamical fractional brownian fields",2005,"soumis")
-*/
-/*ajouter les infos pour les journaux
-insert into revue values (1,"Systèmes d’information et Management");
-insert into revue values (2,"Reliability Engineering & System Safety");
-insert into revue values (3,"Neurocomputing");
-insert into revue values (4,"Proceedings of the Ninth International Conference on Mathematical Methods in Reliability");
-insert into revue values (5,"Proceedings of the European Navigation Conference");
-insert into revue values (6,"PHM");
-insert into revue values (7,"IEEE");
-insert into revue values (8,"Journal de la Société Française de Statistique")
+/*ajouter les infos pour les auteurs
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (1, "Allais","Romain");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (2, "Allouche","Sylvie");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (3, "Aguert","Marc");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (4, "Hachet","Laurent");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (5, "Araud","Christian");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (6, "Atifi","Hassan");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (7, "Begriche","Philippe");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (8, "Belamri","Philippe");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (9, "Belamri","Lucile");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (10, "Bennis","Bruno");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (11, "Billard","Jean-louis");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (12, "Brecht","Daniel");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (13, "Brasil","Sophie");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (14, "Bullot","Florie");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (15, "Casas","Michel");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (16, "Castro","Rafeal");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (17, "Chabbat","Elodie");
+insert into chercheur (id_ch,nom_ch,prenom_ch) values (18, "Châtelet","Luc")
 */
 
-/*ajouter les infos pour organisation*/
-insert into affiliation (no_aff,nom_a) values (1,"CREIDD");
-insert into affiliation (no_aff,nom_a) values (2,"ERA");
-insert into affiliation (no_aff,nom_a) values (3,"GAMMA3");
-insert into affiliation (no_aff,nom_a) values (4,"LASMIS");
-insert into affiliation (no_aff,nom_a) values (5,"LM2S");
-insert into affiliation (no_aff,nom_a) values (6,"LNIO");
-insert into affiliation (no_aff,nom_a) values (7,"LOSI");
-insert into affiliation (no_aff,nom_a) values (8,"Tech-CICO");
+/* insert ne fonctionne pas donc on utilise update
+insert into chercheur (id_ch, organisation) values (1,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (2,"Université de Technologie de Compiègne");
+insert into chercheur (id_ch, organisation) values (3, "Université de Technologie de Belfor-Montbeliard");
+insert into chercheur (id_ch, organisation) values (4,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (5,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (6,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (7,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (8,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (9,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (10,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (11,"Université de Technologie de Belfor-Montbeliard");
+insert into chercheur (id_ch, organisation) values (12,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (13,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (14,"Université de Technologie de Troyes");
+insert into chercheur (id_ch, organisation) values (15,"Université de Technologie de Belfor-Montbeliard");
+insert into chercheur (id_ch, organisation) values (16,"Université de Technologie de Compiègne");
+insert into chercheur (id_ch, organisation) values (17,"Université de Technologie de Compiègne");
+insert into chercheur (id_ch, organisation) values (18,"Université de Technologie de Troyes");
+*/
+
+/*ajouter les infos pour Publications
+
+insert into publication (id_p, titre, annee,lieu_conference) values (1,"Communautés de pratique et performance",2011,"Shanghai");
+insert into publication (id_p, titre, annee,lieu_conference) values (2,"A model to quantify the resilience of mass railway transportation systems",2012,"Paris");
+insert into publication (id_p, titre, annee,lieu_conference) values (3,"Multi-task learning with one-class SVM",2013,"Pekin");
+insert into publication (id_p, titre, annee,titre_conference,lieu_conference) values (4,"Analyse des données pour la notation de lots",2014,"IEEE International Conference on Image Processing","New York");
+insert into publication (id_p, titre, annee,lieu_conference) values (5,"Controlled systems, failure prediction and maintenance",2015,"Tokyo");
+insert into publication (id_p, titre, annee,lieu_conference) values (6,"Multi-Constellation GNSS based on next generation RAIM",2016,"Lyon");
+insert into publication (id_p, titre, annee,titre_conference,lieu_conference) values (7,"A deterioration model for feedback control systems with random environment",2010,"22nd Annual Conference ESREL on European Safety and Reliability","Lille");
+insert into publication (id_p, titre, annee,titre_conference,lieu_conference) values (8,"Degradation prognosis based on a model of Gamma process mixture",2009,"Proc. 24th IEEE workshop on Machine Learning for Signal Processing","Shanghai");
+insert into publication (id_p, titre, annee,lieu_conference) values (9,"Mahalanobis-Based One-Class Classification",2008,"Paris");
+insert into publication (id_p, titre, annee,titre_conference,lieu_conference) values (10,"Privacy Threats from Social Networking Service Aggregators",2007,"5th Cybercrime and Trustworthy Computing Conference","Lyon");
+insert into publication (id_p, titre, annee,lieu_conference) values (11,"Rich Model for Steganalysis of Color Images",2006,"Shanghai");
+insert into publication (id_p, titre, annee,lieu_conference) values (12,"Simulation of image time series from dynamical fractional brownian fields",2005,"Paris") */
+
+
+
+
+/*ajouter les infos pour organisation
+insert into equipe (id_eq,nom_eq) values (1,"CREIDD");
+insert into equipe (id_eq,nom_eq) values (2,"ERA");
+insert into equipe (id_eq,nom_eq) values (3,"GAMMA3");
+insert into equipe (id_eq,nom_eq) values (4,"LASMIS");
+insert into equipe (id_eq,nom_eq) values (5,"LM2S");
+insert into equipe (id_eq,nom_eq) values (6,"LNIO");
+insert into equipe (id_eq,nom_eq) values (7,"LOSI");
+insert into equipe (id_eq,nom_eq) values (8,"Tech-CICO");*/
+
+/*aouter les catégories*/
+insert into categorie (id_cat, nom_cat) values (1, "Article dans des Revues Internationales(RI)");
+insert into categorie (id_cat, nom_cat) values (2, "Article dans des Conférences Internationales(CI)");
+insert into categorie (id_cat, nom_cat) values (3, "Article dans des Revues Françaises(RF)");
+insert into categorie (id_cat, nom_cat) values (4, "Article dans des Conférences Françaises(CF)");
+insert into categorie (id_cat, nom_cat) values (5, "Ouvrages Scientifiques");
+insert into categorie (id_cat, nom_cat) values (6, "Thèse de Doctorat");
+insert into categorie (id_cat, nom_cat) values (7, "Brevet");
+insert into categorie (id_cat, nom_cat) values (8, "Autre Production");
 
 
